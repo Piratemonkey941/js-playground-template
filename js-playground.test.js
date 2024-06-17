c4708ad164c938e3251575812f565a52bf7d1142
Anthony Rodriguez <arodriguez@warehqlabs.com>
2024-06-17 12:36:58 -0500

    basic js algo setup

---
+++ js-playground.test.js
@@ -0,0 +1,12 @@
+import { describe, expect, it } from "vitest";
+
+const main = () => {
+};
+describe("Duplicate Arguments", function () {
+  it("test", () => {
+    expect(main()).toEqual();
+  });
+});
+
+//ReadMe to get this to work install vitest 
+// running -> npm run test <- will run the code locally
